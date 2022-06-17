965d0887902e2d9c73de1d7dd1fc785e1defdc3a
Pedro Clerici <pedro.clerici@tutamail.com>
2022-06-17 16:26:15 -0300

    Implemented score

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import Card from './card.js';
 
 // Global Variables.
 const deck = document.querySelector('.deck');
+let stars = 3
 let cardFlipOne = null;
 let cardFlipTwo = null;
 let matches = 0;
@@ -9,6 +10,7 @@ let moves = 0;
 let cards = [];
 let baseEmojis = ['cherries', 'coconut', 'feather', 'ice', 'rabbit', 'star-struck', 'toothbrush', 'wood'];
 let emojis = baseEmojis.concat(baseEmojis);
+
 
 // Randomizes the emojis array order.
 emojis.sort(() => Math.random() - 0.5);
@@ -71,9 +73,29 @@ cards.forEach(card => {
       }
 
       moves++;
-      if (matches === baseEmojis.length) {
-        alert('YOU WIN!');
-      }
+      updateMoves();
+      updateStars();
+    }
+
+    if (matches === baseEmojis.length) {
+      alert('YOU WIN!');
     }
   })
 })
+
+function updateStars() {
+  const starsNodes = document.querySelectorAll('.score__star > img');
+
+  if (moves >= 16 && moves < 26) {
+    starsNodes[2].setAttribute('src', 'img/stars/black-star.svg');
+    stars = 2;
+  } else if (moves >= 26) {
+    starsNodes[1].setAttribute('src', 'img/stars/black-star.svg');
+    stars = 1;
+  }
+}
+
+function updateMoves() {
+  const movesNode = document.querySelector('.score__moves');
+  movesNode.innerText = moves;
+}
